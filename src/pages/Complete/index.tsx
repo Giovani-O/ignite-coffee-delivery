98edaae57388352ceb324825862a9b1e513f9b8c
Giovani <giovani98@proton.me>
2024-01-11 20:28:20 -0300

    wip: Tela de entrega

--- src/pages/Complete/index.tsx
+++ src/pages/Complete/index.tsx
@@ -1,3 +1,43 @@
+import { CompleteOrderContainer, CompleteOrderInfo } from './styles'
+import Delivery from '../../assets/images/delivery.svg'
+import { CurrencyDollar, MapPin, Timer } from '@phosphor-icons/react'
+
 export function Complete() {
-  return <h1>Complete</h1>
+  return (
+    <CompleteOrderContainer>
+      <h1>Uhu! Pedido confirmado</h1>
+      <h3>Agora é só aguardar que logo o café chegará até você</h3>
+
+      <CompleteOrderInfo>
+        <section>
+          <span>
+            <span>
+              <MapPin weight="fill" />
+            </span>
+            <p>
+              Entrega em <strong>{'Rua Luís Camilo de Camargo, 102'}</strong>
+              <br />
+              {' Centro - Hortolândia, SP'}
+            </p>
+          </span>
+          <span>
+            <span>
+              <Timer weight="fill" />
+            </span>
+            Previsão de entrega
+            <br /> <strong>20 min - 30 min</strong>
+          </span>
+          <span>
+            <span>
+              <CurrencyDollar />
+            </span>
+            Pagamento na entrega
+            <br /> <strong>Cartão de crédito</strong>
+          </span>
+        </section>
+
+        <img src={Delivery} alt="Pessoa de moto fazendo a entrega" />
+      </CompleteOrderInfo>
+    </CompleteOrderContainer>
+  )
 }
---
+++ src/pages/Complete/styles.ts
@@ -0,0 +1,59 @@
+import styled from 'styled-components'
+
+export const CompleteOrderContainer = styled.main`
+  margin: 5rem 10rem;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 2rem;
+    font-weight: 800;
+    line-height: 130%;
+    color: ${(props) => props.theme['yellow-dark']};
+  }
+
+  h3 {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.25rem;
+    font-weight: 400;
+    line-height: 130%;
+    color: ${(props) => props.theme['base-subtitle']};
+  }
+`
+export const CompleteOrderInfo = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 6.375rem;
+  margin-top: 2.5rem;
+
+  section {
+    display: flex;
+    flex-direction: column;
+    padding: 2.8125rem 2.5rem;
+
+    // Adição de gradiente à borda
+    border: double 1px transparent; // 1
+    border-radius: 6px 36px 6px 36px; // 2
+    background-image:
+      linear-gradient(
+        ${(props) => props.theme.background},
+        ${(props) => props.theme.background}
+      ),
+      // 3
+      linear-gradient(
+          to right,
+          ${(props) => props.theme.yellow},
+          ${(props) => props.theme.purple}
+        ); // 4
+    background-origin: border-box; // 5
+    background-clip: padding-box, border-box; // 6
+    // 1 - Crio uma borda dupla transparente
+    // 2 - Defino o radius da borda
+    // 3 - Defino a cor de background com 2 gradientes, o primeiro é a cor de fundo
+    // 4 - O segundo gradiente é a cor da borda
+    // 5 - O background vai começar na borda da section
+    // 6 - background-clip define onde o background da section vai ser visível
+  }
+`
